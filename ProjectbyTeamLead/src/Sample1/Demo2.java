020205db2d799c55164a1155a91857ee8c996249
hdesale@rdalabs.com <hdesale@rdalabs.com>
2023-05-05 17:06:50 +0530

    Commiting the updated code (added the nested if and else)

--- ProjectbyTeamLead/src/Sample1/Demo2.java
+++ ProjectbyTeamLead/src/Sample1/Demo2.java
@@ -6,10 +6,21 @@ public class Demo2
 	public static void main(String[] args)
 	{
 		int marks = 23;
+		String UN = "HiteshDesale";
+		String PWD = "Test@1234";
 		
 		if(marks <= 25)
 		{
 			System.out.println("Student is pass");
+			if (UN == "HiteshDesale")
+			{
+				System.out.println("UN is correct, ask him to enter pwd");
+			}
+			else
+			{
+				System.out.println("PWD is correct");
+				System.out.println("Login successfully");
+			}
 		}
 		else
 		{
