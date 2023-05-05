830d4888d919f5f761aa7a115eaae3f84b87dab4
hdesale@rdalabs.com <hdesale@rdalabs.com>
2023-05-05 16:49:43 +0530

    Pushing to branchY

---
+++ ProjectbyTeamLead/src/Sample1/Demo2.java
@@ -0,0 +1,27 @@
+package Sample1;
+
+//@Author- HiteshQAEngineer
+public class Demo2 
+{
+	public static void main(String[] args)
+	{
+		int marks = 23;
+		
+		if(marks <= 25)
+		{
+			System.out.println("Student is pass");
+		}
+		else
+		{
+			System.out.println("Student is fail");
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
