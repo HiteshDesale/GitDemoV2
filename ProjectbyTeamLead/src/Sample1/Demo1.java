a3ec36d870b896b7965e31aeed982ccc27011a2a
hdesale@rdalabs.com <hdesale@rdalabs.com>
2023-05-05 16:43:50 +0530

    First commit to master branch by HiteshDesale TeamLead

---
+++ ProjectbyTeamLead/src/Sample1/Demo1.java
@@ -0,0 +1,27 @@
+package Sample1;
+
+//@Author- HiteshDesale Team Lead
+public class Demo1 
+{
+	public static void main(String[] args)
+	{
+		int marks = 23;
+		
+		if(marks <= 25)
+		{
+			System.out.println("Student is pass");
+		}
+		else
+		{
+			System.out.println("Student is fail");
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
